aed68065cd5381dafd743839817db613e4e18c3f
VK <vitaliy.krynytskyy@gmail.com>
2019-11-09 18:16:42 +0000

    Added ability to see drinks for each brand

--- client/src/components/Drinks/index.js
+++ client/src/components/Drinks/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Strapi from "strapi-sdk-javascript";
+import { Box, Heading, Text, Image, Card, Button } from "gestalt";
 
 const apiURL = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiURL);
 
 class Drinks extends React.Component {
+  state = {
+    drinks: [],
+    brand: ""
+  };
+
   async componentDidMount() {
     try {
       const { data } = await strapi.request("POST", "/graphql", {
@@ -17,6 +23,7 @@ class Drinks extends React.Component {
                           drinks {
                               id
                               name
+                              description
                               image {
                                   name
                                   url
@@ -29,14 +36,70 @@ class Drinks extends React.Component {
         }
       });
 
-      console.log(data);
+      this.setState({ drinks: data.brand.drinks, brand: data.brand.name });
     } catch (err) {
       console.error(err);
     }
   }
 
   render() {
-    return <div>Drinks</div>;
+    const { brand, drinks } = this.state;
+
+    return (
+      <Box
+        marginTop={4}
+        display="flex"
+        justifyContent="center"
+        alignItems="start"
+      >
+        <Box display="flex" direction="column" alignItems="center">
+          <Box margin={2}>
+            <Heading color="orchid">{brand}</Heading>
+          </Box>
+
+          <Box wrap display="flex" justifyContent="center" padding={4}>
+            {drinks.map(drink => (
+              <Box key={drink.id} width={210} margin={2} paddingY={4}>
+                <Card
+                  image={
+                    <Box height={250} width={200}>
+                      <Image
+                        fit="cover"
+                        alt={drink.name}
+                        naturalHeight={1}
+                        naturalWidth={1}
+                        src={`${apiURL}${drink.image.url}`}
+                      />
+                    </Box>
+                  }
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    direction="column"
+                  >
+                    <Box marginBottom={2}>
+                      <Text bold size="xl">
+                        {drink.name}
+                      </Text>
+                    </Box>
+
+                    <Text>{drink.description}</Text>
+                    <Text color="orchid">£{drink.price}</Text>
+                    <Box marginTop={2}>
+                      <Text bold size="xl">
+                        <Button color="blue" text="Add to cart" />
+                      </Text>
+                    </Box>
+                  </Box>
+                </Card>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    );
   }
 }
 
