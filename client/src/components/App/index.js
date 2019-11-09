66717278e9f5b83af511e464377b9b8cef2455b6
VK <vitaliy.krynytskyy@gmail.com>
2019-11-09 11:50:34 +0000

    Added fetching of data from back end

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,17 +1,44 @@
 import React from "react";
 import { Container, Box, Heading } from "gestalt";
 import "./App.css";
+import Strapi from "strapi-sdk-javascript";
 
-function App() {
-  return (
-    <Container>
-      <Box display="flex" justifyContent="center" marginBottom={2}>
-        <Heading color="midnight" size="md">
-          Brands
-        </Heading>
-      </Box>
-    </Container>
-  );
+const apiURL = process.env.API_URL || "http://localhost:1337";
+
+const strapi = new Strapi(apiURL);
+
+class App extends React.Component {
+  async componentDidMount() {
+    const response = await strapi.request("POST", "/graphql", {
+      data: {
+        query: `{
+					brands {
+						id
+						name
+						description
+						createdAt
+						image {
+							url
+							name
+						}
+					}
+				}`
+      }
+    });
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Box display="flex" justifyContent="center" marginBottom={2}>
+          <Heading color="midnight" size="md">
+            Brands
+          </Heading>
+        </Box>
+      </Container>
+    );
+  }
 }
 
 export default App;
