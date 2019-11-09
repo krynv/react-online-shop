ceaf3573b35cfc258bec192a8c3aca0304fb1d40
VK <vitaliy.krynytskyy@gmail.com>
2019-11-09 15:28:29 +0000

    Added brands and links to the landing page

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Container, Box, Heading } from "gestalt";
-import "./App.css";
+import { Container, Box, Heading, Card, Image, Text } from "gestalt";
 import Strapi from "strapi-sdk-javascript";
+import { Link } from "react-router-dom";
+
+import "./App.css";
 
 const apiURL = process.env.API_URL || "http://localhost:1337";
-
 const strapi = new Strapi(apiURL);
 
 class App extends React.Component {
@@ -41,12 +42,48 @@ class App extends React.Component {
   }
 
   render() {
+    const { brands } = this.state;
+
     return (
       <Container>
         <Box display="flex" justifyContent="center" marginBottom={2}>
           <Heading color="midnight" size="md">
             Brands
           </Heading>
+        </Box>
+
+        <Box display="flex" justifyContent="around">
+          {brands.map(brand => (
+            <Box key={brand.id} width={200} margin={2}>
+              <Card
+                image={
+                  <Box height={200} width={200}>
+                    <Image
+                      alt={brand.name}
+                      naturalHeight={1}
+                      naturalWidth={1}
+                      src={`${apiURL}${brand.image.url}`}
+                    />
+                  </Box>
+                }
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Text bold size="xl">
+                    {brand.name}
+                  </Text>
+                  <Text>{brand.description}</Text>
+                  <Text bold size="xl">
+                    <Link to={`/${brand.id}`}>See their beers</Link>
+                  </Text>
+                </Box>
+              </Card>
+            </Box>
+          ))}
         </Box>
       </Container>
     );
