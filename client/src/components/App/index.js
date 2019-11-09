a421bfc4595f454ff9992b46988365f25dc458ca
VK <vitaliy.krynytskyy@gmail.com>
2019-11-09 10:55:37 +0000

    Added a heading for the WIP brewery brands landing page

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Container, Box, Heading } from "gestalt";
+import "./App.css";
 
 function App() {
-    return (
-        <div>
-            App
-        </div>
-    );
+  return (
+    <Container>
+      <Box display="flex" justifyContent="center" marginBottom={2}>
+        <Heading color="midnight" size="md">
+          Brands
+        </Heading>
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
